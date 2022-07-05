b8e1d0ff7be2631933e95f78db0dedf57c43e079
Subhankar Das <subhankardas831@gmail.com>
2022-07-05 18:41:32 +0530

    Added code for panic and recover

--- learning/advanced/1.4advanced.go
+++ learning/advanced/1.4advanced.go
@@ -2,6 +2,7 @@ package advanced
 
 import (
 	"fmt"
+	"os"
 	"sync"
 )
 
@@ -42,4 +43,63 @@ func Advanced11() {
 
 	wg.Wait()
 	fmt.Println("counters", con.counters)
+}
+
+/* A panic typically means something went unexpectedly wrong. Mostly we use it to
+fail fast on errors that shouldn’t occur during normal operation, or that we aren’t
+prepared to handle gracefully. */
+func Advanced12() {
+	_, err := os.Create("/tmp/file")
+	if err != nil {
+		// Abort if a function returns an error value that we don’t want to handle.
+		panic("unable to create file")
+	}
+}
+
+/* Defer is used to ensure that a function call is performed later in a program’s
+execution, usually for purposes of cleanup. */
+func Advanced13() {
+	file := createFile()
+	writeFile(file)
+
+	// This will be executed at the end of the enclosing function, after writeFile has finished
+	defer closeFile(file)
+}
+
+func createFile() *os.File {
+	f, err := os.Create("file.txt")
+	if err != nil {
+		panic("unable to create file")
+	}
+	return f
+}
+
+func writeFile(f *os.File) {
+	fmt.Fprintln(f, "file data")
+}
+
+func closeFile(f *os.File) {
+	err := f.Close()
+	if err != nil {
+		panic("unable to close file")
+	}
+}
+
+/* Recover - from a panic, by using the recover built-in function. A recover can stop a
+panic from aborting the program and let it continue with execution instead. */
+func doPanic() {
+	panic("some error")
+}
+
+func Advanced14() {
+	defer func() {
+		// Recover must be called within a deferred function. When the enclosing function
+		// panics, the defer will activate and a recover call within it will catch the panic.
+		if r := recover(); r != nil {
+			fmt.Println("recover", r) // Return value of recover is the error raised in the call to panic
+		}
+	}()
+
+	doPanic()                           // Simulate some logic that will panic
+	fmt.Println("after panic occurred") // This is never printed as panic will trigger the defer function
 }
